a2868815de9da491a94abbb8bee09bc68f575cf5
Lane Schwartz <dowobeha@gmail.com>
2025-04-01 17:20:35 -0400

    Attempt Swift package

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AkuzipikDigitalCorpus",
+    products: [
+        .library(name: "AkuzipikDigitalCorpus",
+                 targets: ["EarlyReaders"]),
+    ],
+    targets: [
+        .target(name: "EarlyReaders",
+                resources: [.process("ess/early_readers"),
+                            .process("eng/early_readers")]
+               )
+    ]
+)
+
